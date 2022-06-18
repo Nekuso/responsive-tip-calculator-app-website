c507f1096bdcb43779b96d495deeea962aa86394
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-06-18 11:33:48 +0800

    added files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 billInput = document.querySelector(".bill__input");
+peopleInput = document.querySelector(".people__input");
 
 tip__buttons = document.querySelectorAll(".tip__buttons button");
 reset = document.querySelector(".reset");
+
+btn5 = document.querySelector(".btn5");
+btn10 = document.querySelector(".btn10");
+btn15 = document.querySelector(".btn15");
+btn25 = document.querySelector(".btn25");
+btn50 = document.querySelector(".btn50");
+costumBtn = document.querySelector(".costumBtn");
 
 
 tipResult = document.querySelector(".tip__result");
@@ -15,7 +23,42 @@ tip__buttons.forEach(btn => {
     });
 });
 
+// TIP BUTTON
+btn5.addEventListener("click", () => {
+    tipResult.innerText = "$" + (billInput.value * (5 / 100)).toFixed(2);
+    totalResult.innerText = "$" + (billInput.value * (5 / 100) * peopleInput.value).toFixed(2);
+})
+btn10.addEventListener("click", () => {
+    tipResult.innerText = "$" + (billInput.value * (10 / 100)).toFixed(2);
+    totalResult.innerText = "$" + (billInput.value * (10 / 100) * peopleInput.value).toFixed(2);
+})
+btn15.addEventListener("click", () => {
+    tipResult.innerText = "$" + (billInput.value * (15 / 100)).toFixed(2);
+    totalResult.innerText = "$" + (billInput.value * (15 / 100)* peopleInput.value).toFixed(2);
+})
+btn25.addEventListener("click", () => {
+    tipResult.innerText = "$" + (billInput.value * (25 / 100)).toFixed(2);
+    totalResult.innerText = "$" + (billInput.value * (25 / 100)* peopleInput.value).toFixed(2);
+})
+btn50.addEventListener("click", () => {
+    tipResult.innerText = "$" + (billInput.value * (50 / 100)).toFixed(2);
+    totalResult.innerText = "$" + (billInput.value * (50 / 100)* peopleInput.value).toFixed(2);
+})
+
+
+costumBtn.addEventListener("keyup", e => {
+    
+    if(e.key == "Enter"){
+        tipResult.innerText = "$" + (billInput.value * (costumBtn.value / 100)).toFixed(2);
+        totalResult.innerText = "$" + (billInput.value * (costumBtn.value / 100) * peopleInput.value).toFixed(2);
+    }
+    
+});
+
 reset.addEventListener('click', () => {
+    billInput.value = "0"
+    peopleInput.value = "0"
+    costumBtn.value = "0"
     tipResult.innerText = "$" + "0";
     totalResult.innerText = "$" + "0";
 })
